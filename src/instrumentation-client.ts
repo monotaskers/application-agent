0880a67c41d00d72fbf31ae4ec31e55faeb962cf
tombeck <tom@ocupop.com>
2025-09-23 17:31:19 -0700

    updated sentry

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,20 +1,18 @@
 // This file configures the initialization of Sentry on the client.
 // The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 if (!process.env.NEXT_PUBLIC_SENTRY_DISABLED) {
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-
     // Add optional integrations for additional features
     integrations: [Sentry.replayIntegration()],
 
-    // Adds request headers and IP for users, for more info visit
-    sendDefaultPii: true,
-
     // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
     tracesSampleRate: 1,
+    // Enable logs to be sent to Sentry
+    enableLogs: true,
 
     // Define how likely Replay events are sampled.
     // This sets the sample rate to be 10%. You may want this to be 100% while
@@ -25,7 +23,7 @@ if (!process.env.NEXT_PUBLIC_SENTRY_DISABLED) {
     replaysOnErrorSampleRate: 1.0,
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false
+    debug: false,
   });
 }
 
